983ed8bdc7d266c8506eda03748fa48c68b05da5
Gustavo Rios <gerquevedo@unsm.edu.pe>
2025-04-18 23:02:00 -0500

    CREACION DE EJERCICIO DE POO

---
+++ src/Principal.java
@@ -0,0 +1,63 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.calculos.FiltroRecomendacion;
+import com.aluracursos.screenmatch.modelos.Episodio;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(18);
+        miPelicula.evalua(7.8);
+        miPelicula.evalua(9);
+
+        System.out.println(miPelicula.getTotalDeEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println("Tiempo necesario para ver tus titulos favoritos estas vacaciones: "+calculadora.getTiempoTotal()+" minutos");
+
+
+        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+        filtroRecomendacion.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setNombre("La Casa Targaryen");
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(50);
+
+        filtroRecomendacion.filtra(episodio);
+
+
+//        otraPelicula.muestraFichaTecnica();
+
+//        System.out.println("Mi otra película es: "+otraPelicula.nombre);
+//        System.out.println("Su fecha de lanzamiento fue: "+otraPelicula.fechaDeLanzamiento);
+
+
+    }
+}
---
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,64 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public int getTotalDeEvaluaciones(){
+        return totalDeEvaluaciones;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la pellícula es: "+getNombre());
+        System.out.println("Su fecha de lanzamiento fue: "+getFechaDeLanzamiento());
+        System.out.println("Duracion en minutos: "+ getDuracionEnMinutos());
+    }
+
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeEvaluaciones++;
+
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeEvaluaciones;
+
+    }
+}
